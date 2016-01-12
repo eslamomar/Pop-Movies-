de25665c50189cfc10d78e64f707620bbbe24f5e
Eslam <eslamomarmoh@gmail.com>
2016-01-13 01:30:47 +0200

    Update API_Key.java

--- app/src/main/java/movieapp/eslamomar/com/mostmovies/API_Key.java
+++ app/src/main/java/movieapp/eslamomar/com/mostmovies/API_Key.java
@@ -4,6 +4,6 @@ package movieapp.eslamomar.com.mostmovies;
  * Created by Eslam on 1/2/2016.
  */
 public class API_Key {
-    public static final String API_KEY = "214a43b9f8ae172761877c87c2fd0c74";
+    public static final String API_KEY = "write_your_api_key_here";
     //API_Key.API_KEY
 }
